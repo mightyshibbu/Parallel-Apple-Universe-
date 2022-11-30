274870f9bada61593df3754070d7cb6ac2128dde
Pranav Tripathi <72650749+mightyshibbu@users.noreply.github.com>
2022-11-30 08:31:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+// import logo from './logo.svg';
+import './App.css';
+import Alert from 'react-bootstrap/Alert';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function App() {
+  return (
+    <>
+
+
+
+
+<Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              align="middle"
+              src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Apple_logo_grey.svg"
+              height="25"
+              className="d-inline-block align-top"
+            />{' '}
+            
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Store</Nav.Link>
+            <Nav.Link href="https://www.apple.com/mac/">Mac</Nav.Link>
+            <Nav.Link href="https://www.apple.com/ipad/">iPad</Nav.Link>
+            <Nav.Link href="https://www.apple.com/iphone/">iPhone</Nav.Link>
+            <Nav.Link href="https://www.apple.com/Watch/">Watch</Nav.Link>  
+            <Nav.Link href="https://www.apple.com/airpods/">AirPods</Nav.Link>
+            <Nav.Link href="https://support.apple.com/">Support</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      {['dark'].map((variant) => (
+        <Alert key={variant} variant={variant}>
+          Introducing Apple One. Bundle your favorite services and enjoy more of
+          Apple for less.{' '}
+          <a href="https://www.apple.com/apple-one/"> Learn more</a>
+        </Alert>
+      ))}
+<h2><center>Apple Inc.</center> </h2>
+
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/App2try.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+
+
+
+
+
+
+
+
+
+    </>
+  );
+}
+
+export default App;
+<div class="mx-auto order-0">
+        <a class="navbar-brand mx-auto" href="#"><Navbar.Brand href="https://www.apple.com/">
+            <img
+              alt=""
+              align="middle"
+              src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Apple_logo_grey.svg"
+              height="25"
+              className="d-inline-block align-top"
+            />{' '}
+          </Navbar.Brand></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
